469afb834177c1af9a991a0d769345b8774b689b
Felix Moore <felix.moore@kainos.com>
2021-08-05 13:16:55 +0100

    Database connection

--- src/com/kainos/ea/HRSystemDB.java
+++ src/com/kainos/ea/HRSystemDB.java
@@ -1,8 +1,14 @@
 package com.kainos.ea;
 
+import com.kainos.ea.employee.Employee;
+
 import java.io.FileInputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
 /**
@@ -12,23 +18,25 @@ import java.util.Properties;
  */
 public class HRSystemDB {
     private static Connection c;
+
     private static Connection getConnection() {
         String user;
         String password;
         String host;
         Connection c;
-        try (var f = new FileInputStream("HRSystemDB.properties")){
+        try (var f = new FileInputStream("HRSystemDB.properties")) {
             Properties props = new Properties();
             props.load(f);
-            user      = props.getProperty("user");
-            password   = props.getProperty("password");
-            host      = props.getProperty("host");
+            user = props.getProperty("user");
+            password = props.getProperty("password");
+            host = props.getProperty("host");
             if (user == null || password == null || host == null)
                 throw new IllegalArgumentException(
                         "Properties file must exist and must contain user, " +
                                 "password, and host properties.");
             c = DriverManager.getConnection("jdbc:mysql://" + host +
-                    "/HRSystemDB_iplusplus", user, password);
+                    "/HRSystem_iplusplus", user, password);
+            System.out.println(c);
             return c;
         } catch (Exception e) {
             e.printStackTrace();
@@ -36,31 +44,30 @@ public class HRSystemDB {
         return null;
     }
 
-//    public static List<Employee> getEmployees(){ //generic get method, to be adapted once database populated
-//            if (c == null) {
-//                c = getConnection();
-//            }
-//            List<Employee> emps = new ArrayList<>();
-//            try {
-//                Statement s = c.createStatement();
-//                ResultSet rows = s.executeQuery(
-//                        """
-//                            SELECT emp_no / 10e3 AS `number`,
-//                                CONCAT_WS(' ', first_name, last_name) AS `name`,
-//                                salary * 100 AS `salary`
-//                            FROM employees JOIN salaries USING(emp_no)
-//                            WHERE to_date > NOW()
-//                            """);
-//                while (rows.next()) {
-//                    emps.add(new Employee(
-//                            rows.getShort("number"),
-//                            rows.getString("name"),
-//                            rows.getInt("salary")));
-//                }
-//            } catch (Exception e) {
-//                e.printStackTrace();
-//            }
-//            return emps;
-//        }
-//    }
+    public static List<Employee> getAllEmployees(){ //generic get method, to be adapted once database populated
+        if (c == null) {
+            c = getConnection();
+        }
+        List<Employee> employees = new ArrayList<>();
+        try {
+            Statement s = c.createStatement();
+            ResultSet rows = s.executeQuery(
+                    "SELECT employee_id AS `ID`,first_name, last_name, nin, salary, department, email, phone_number FROM Employee;");
+            while (rows.next()) {
+                employees.add(new Employee(
+                        rows.getInt("ID"),
+                        rows.getString("first_name"),
+                        rows.getString("last_name"),
+                        rows.getString("nin"),
+                        rows.getFloat("salary"),
+                        rows.getString("department"),
+                        rows.getString("email"),
+                        rows.getString("phone_number")
+                ));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return employees;
+    }
 }
--- src/com/kainos/ea/Main.java
+++ src/com/kainos/ea/Main.java
@@ -1,13 +1,22 @@
 package com.kainos.ea;
-
+import com.kainos.ea.employee.Employee;
+import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+
+        //basic db conn code
+        long t0, t1, t2;
+        t0 = System.nanoTime();
+        List<Employee> bigEmps = HRSystemDB.getAllEmployees();
+        t1 = System.nanoTime();
+
+        t2 = System.nanoTime();
+        System.err.println(bigEmps.size() + " elements in the list.");
+        System.err.println("Query took "
+                + (t1-t0) + " nanos.");
+        System.err.println("Streaming took "
+                + (t2-t1) + " nanos.");
     }
 }
-
-
-
-
-
