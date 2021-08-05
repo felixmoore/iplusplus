38684e6e7855f934246198ecf6f5ec6cab17c18e
sylwia-luczak <88457687+sylwia-luczak@users.noreply.github.com>
2021-08-05 17:10:09 +0200

    Sqlscript (#3)
    * add sql script
    
    * add salary to employee
    
    * splited sql scripts

---
+++ sql/HRSystem_iplusplus_dropDB.sql
@@ -0,0 +1,14 @@
+USE HRSystem_iplusplus;
+
+DROP TABLE IF EXISTS Technical;
+
+DROP TABLE IF EXISTS Projects;
+
+DROP TABLE IF EXISTS Customer;
+
+DROP TABLE IF EXISTS Sales;
+
+DROP TABLE IF EXISTS BankDetails;
+
+DROP TABLE IF EXISTS Employee;
+
