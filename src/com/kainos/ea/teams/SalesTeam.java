20116cae5d8d2f767d80ca97d5fb246ea71264a1
Aimee Boyle <aimee.boyle@kainos.com>
2021-08-05 13:21:44 +0100

    sales team structure

--- src/com/kainos/ea/teams/SalesTeam.java
+++ src/com/kainos/ea/teams/SalesTeam.java
@@ -1,4 +1,17 @@
 package com.kainos.ea.teams;
+import com.kainos.ea.employee.SalesEmployee;
+import java.util.ArrayList;
 
 public class SalesTeam {
+    private ArrayList<SalesEmployee> allSalesEmployees;
+
+    public void handleUserInput(){
+
+    }
+
+    public void getAllSalesEmployees(){
+        if (c == null) {
+//                c = getConnection();
+//            }
+    }
 }
