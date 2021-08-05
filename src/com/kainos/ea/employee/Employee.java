ac83a2b552a9fa5f7d84e801b451ba483ec9777b
Amir Makanvand <amir.makanvand@kainos.com>
2021-08-05 12:15:28 +0100

    add employee class

--- src/com/kainos/ea/employee/Employee.java
+++ src/com/kainos/ea/employee/Employee.java
@@ -1,4 +1,90 @@
 package com.kainos.ea.employee;
 
 public class Employee {
+    protected int employee_id;
+    protected String firstName;
+    protected String lastName;
+    protected String nationalInsurance;
+    protected float salary;
+    protected String department;
+    protected String email;
+    protected String phoneNumber;
+
+    public Employee(int employee_id, String firstName,
+                    String lastName, String nationalInsurance,
+                    float salary, String department,
+                    String email, String phoneNumber) {
+        this.employee_id = employee_id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.nationalInsurance = nationalInsurance;
+        this.salary = salary;
+        this.department = department;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getEmployee_id() {
+        return employee_id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getNationalInsurance() {
+        return nationalInsurance;
+    }
+
+    public float getSalary() {
+        return salary;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setEmployee_id(int employee_id) {
+        this.employee_id = employee_id;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setNationalInsurance(String nationalInsurance) {
+        this.nationalInsurance = nationalInsurance;
+    }
+
+    public void setSalary(float salary) {
+        this.salary = salary;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 }
